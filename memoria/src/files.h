79444067e98cff3b441770b5127bef46e4baac2d
utnso <jannet7mil@gmail.com>
2024-06-20 04:17:59 +0000

    pid and dicc

--- memoria/src/files.h
+++ memoria/src/files.h
@@ -17,8 +17,16 @@ typedef struct {
     int capacity;
 } InstructionDictionary;
 
-extern InstructionDictionary dict;
+typedef struct {
+    uint32_t pid;
+    InstructionDictionary *instruction_dict;
+} PIDToDict;
 
+#define MAX_PROCESSES 100
+
+extern PIDToDict pid_dict_array[MAX_PROCESSES];
+
+extern InstructionDictionary dict;
 
 FILE *open_file(const char *file_path);
 void handle_create_process(const char *file_path, uint32_t pid);
@@ -27,8 +35,11 @@ void instructions_put(InstructionDictionary *dict, const char *complete_line);
 void instruction_dictionary_free(InstructionDictionary *dict);
 void load_instructions_from_file(InstructionDictionary *dict, FILE *file);
 const char *get_complete_instruction(const InstructionDictionary *dict, int index);
+//const char *get_complete_instruction(uint32_t pid, int index);
+
 FILE *open_file(const char *file_path);
 void send_instruction(const char *instruction, int socket_cliente);
+void add_pid_instruction_dict(uint32_t pid, InstructionDictionary *dict);
 
 
 
