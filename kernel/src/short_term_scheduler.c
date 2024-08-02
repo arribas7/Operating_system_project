f8a3e338cdca549e85285f106b452ba96c59356c
GabrielJuarez <gajuarez@frba.utn.edu.ar>
2024-08-02 17:27:22 +0000

    Cpu / mem leaks in 0, quantum fixed

--- kernel/src/short_term_scheduler.c
+++ kernel/src/short_term_scheduler.c
@@ -311,8 +311,8 @@ void st_sched_ready_running(void* arg) {
         t_return_dispatch *ret = cpu_dispatch(pcb_RUNNING, config);
         log_debug(logger, "Dispatched a PCB %s", next_pcb->path);
 
-        //sem_wait(&sem_cpu_dispatch); //continue if scheduler is ready
-        //sem_post(&sem_cpu_dispatch); //scheduler is ready
+        sem_wait(&sem_cpu_dispatch); //continue if scheduler is ready
+        sem_post(&sem_cpu_dispatch); //scheduler is ready
         // Handle errors from the CPU dispatch
         if(ret->resp_code == GENERAL_ERROR || ret->resp_code == NOT_SUPPORTED){
             log_error(logger, "Error returned from cpu dispatch: %d", ret->resp_code);
